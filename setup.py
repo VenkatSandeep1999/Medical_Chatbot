8babcbd9c491c9ec282b889243da2d8e1300ca5d
Venkat_Sandeep <vgadi4@unh.newhaven.edu>
2026-04-06 20:39:04 +0530

    Added chatbot UI and backend

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot",
+    version="0.10.0",
+    author="Venkat_Sandeep_Gadi",
+    author_email="gadivenkatsandeep@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
